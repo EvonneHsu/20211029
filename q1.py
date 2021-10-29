85cc9e22921a819dd9c5c6303d976d2e8f5afbe3
許亦佳 <91245625+EvonneHsu@users.noreply.github.com>
2021-10-29 10:51:16 +0800

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,10 @@
+n=int(input())
+var =False
+bool=False
+for i in range(1,n):
+    sum=0
+    for j in range(i,j)
+        print('{}+'.format(k),end="")
+        print(j)
+if bool==var:
+    print("No")
