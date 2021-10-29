140e076f2fbdc9bcc445ef7835b791feaa945ce3
許亦佳 <91245625+EvonneHsu@users.noreply.github.com>
2021-10-29 10:53:59 +0800

    Create q6.py

---
+++ q6.py
@@ -0,0 +1,7 @@
+decimal = 13
+binary = bin(decimal)
+octal = oct(decimal)
+hexadecimal = hex(decimal)
+print('Bin => Dec:', int(binary, 2))
+print('Oct => Dec:', int(octal, 8))
+print('Hex => Dec:', int(hexadecimal, 16))
